a9f25696b8d2ce7f4ec1a89355f3158f3fbb641a
Daniil <dd.shchepelin@gmail.com>
2017-09-04 17:58:35 +0300

    class was added

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -8,10 +8,25 @@ namespace SnakeGame
 {
     class Program
     {
+       /* static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        } */
         static void Main(string[] args)
         {
-            // hello world
-            Console.WriteLine("Hello world");
+            CPoint p1 = new CPoint();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            CPoint p2 = new CPoint();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+            Console.ReadLine();
         }
     }
 }
